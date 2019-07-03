ca0d218baf98ce87d2eb9a706c8eca45b4764a2d
Yao-Chung Chen <40789913+ferygood@users.noreply.github.com>
2019-07-03 17:50:59 +0800

    Add files via upload
    add annotation

--- asus_vivo_watch_bp/omnicare_tools/bloodpressure.py
+++ asus_vivo_watch_bp/omnicare_tools/bloodpressure.py
@@ -7,55 +7,56 @@ import numpy as np
 
 class Blood_pressure():
     
-    # inital function
     def __init__(self, csv_file):
-        # read blood pressure csv file 
+        # initial function
+        # read blood pressure csv file download from OmniCare website 
         self.df = pd.read_csv(csv_file)
     
-    # showhead function is based on pandas function
+    
     def showhead(self, rows):
-        # show rows of dataframe    
-        df = self.df.head(rows)
+        # This function is based on pandas .head() function
+        # show rows of dataframe, input arbitary row number    
+        df = self.df.head(rows) 
         print (df)
     
-    # data preprocess function 
-    def preprocess(self,name):
+     
+    def preprocess(self,name): 
         '''
-        1. select the columns of interest
-        2. eliminate rows when value = -1
-        3. adjust 'time' (hours only)
-        4. save clean dataframe to csv file with customized name (name)     
+        Save and manipulate data we are interested and save it to a csv file.
+        Input: bp_hr/raw/blood_pressure.csv download from OmniCare website
+        Output: save the cleaned.csv file in /bp_hr/clean 
         '''
         self.name = name # customized name
-        df = self.df[['hr','time','sys','dia']] # select specific columns
-        df = df[df.sys != -1] # remove rows by specific 
-        df["original_id"] = df.index
-        df["time"] = df["time"].apply(lambda x: int(x.split()[1].split(":")[0]))
-        df.to_csv('bp_hr/clean/%s_clean.csv'%(name), index=False) # save csv file
+        df = self.df[['hr','time','sys','dia']] # select specific interest columns
+        df = df[df.sys != -1] # remove rows when value = -1 
+        df["original_id"] = df.index # reset index
+        df["time"] = df["time"].apply(lambda x: int(x.split()[1].split(":")[0])) # adjust 'time' (hours only)
+        df.to_csv('bp_hr/clean/%s_clean.csv'%(name), index=False) # save cleaned.csv file
         self.clean_csv = pd.read_csv('bp_hr/clean/%s_clean.csv'%(name))
-    
-    # function of calculation mean of the blood pressure and heart rate 
+
+
     def mean_table(self, df = pd.DataFrame(), group = "time", showTable = False, row = 10):
         '''
-        1. calculate mean of the blood pressure and the heart rate
-        2. return pandas dataframe with group table
+        Calculate mean blood pressure and heart rate of preprocess() output
+        Input: preprocess() output
+        Output: return pandas dataframe with mean blood pressure & heart rate by group 
         '''
         #print(type(time)) # check the data type 
         #print(time.size()) # to know how large is the data in each column
         #print(time.groups) # to know the details of each column and data
         #print(time.get_group(12)) # get specific group e.g.12,
-        if df.empty:
+        if df.empty:               # if df is empty (no input), then it will take the output of preprocess() 
             df = self.clean_csv
-        table = df.groupby(group)
-        if showTable:
+        table = df.groupby(group)  
+        if showTable:              # showTable = True
             print(table.head(row))
         return table.mean()
     
-    # function of calculation stndarf deviation of the blood pressure and the heart rate
     def standard_deviation_table(self, df = pd.DataFrame(), group = "time", showTable = False, row = 10):
         '''
-        1. calculate standard deviation of the blood pressure and the heart rate
-        2. return pandas dataframe with group table
+        Calculate standard deviation (std) of blood pressure and heart rate
+        Input: preprocess() output
+        Output: return pandas dataframe with std of blood pressure & heart rate by group
         '''
         #print(type(time)) # check the data type 
         #print(time.size()) # to know how large is the data in each column
@@ -69,8 +70,12 @@ class Blood_pressure():
             print(standardDeviation.head(row))
         return standardDeviation
         
-    #Draw a lineplot: x-axis = hour; y-axis = bp and hr(average sys and dia)
     def lineplot(self, saveFile = False, showImage = True, fileName = "lineplot"):
+        '''
+        lineplot; x-axis = hour; y-axis = bp and hr (average sys and dia); with figure lengend
+        Input: preprocess() output
+        Output: bp_hr/.png figure file
+        '''
         table = self.mean_table() #calculate the mean of each time group
         #print(table['hr']) #you can print a single column
         
@@ -78,7 +83,7 @@ class Blood_pressure():
         plt.plot(table['sys'], label='systolic bp')
         plt.plot(table['dia'], label='diastolic bp')
         plt.plot(table['hr'], label='heart rate')
-        plt.legend(loc='lower right')
+        plt.legend(loc='lower right', bbox_to_anchor=(1.4, 0.2))
         plt.xlabel('Time(hour)')
         plt.ylabel('mmHg, heart rate/min')
         plt.xticks(np.linspace(0,23,24))
@@ -86,21 +91,25 @@ class Blood_pressure():
             plt.savefig('bp_hr/fig/%s_bp_hr_%s'%(self.name, fileName))
         if showImage:
             plt.show()
-        plt.clf()
+        plt.clf() # clean the figure
         return
     
-        #Draw a lineplot: x-axis = hour; y-axis = bp and hr(average sys and dia)
-    def errorbar(self, saveFile = False, showImage = True, fileName = "lineplot"):
-        table = self.mean_table() #calculate the mean of each time group
-        standardDeviation = self.standard_deviation_table()
+    def errorbar(self, saveFile = False, showImage = True, fileName = "errorbar"):
+        '''
+        lineplot with errorbar; x-axis = hour; y-axis = bp and hr (average sys and dia); with figure legend
+        Input: preprocess() output
+        Output: bp_hr/.png figure file
+        '''
+        table = self.mean_table() #calculate the mean in each time group
+        standardDeviation = self.standard_deviation_table() #calcualte the std in each time group
         #print(table['hr']) #you can print a single column
-        
+
         plt.title("%s 24 Hours Blood Pressure Plot"%(self.name))
         #plt.errorbar(x, y + 3, yerr=yerr, label='both limits (default)')
         plt.errorbar(table.index, table['sys'], yerr = standardDeviation['sys'], label='systolic bp')
         plt.errorbar(table.index, table['dia'], yerr = standardDeviation['dia'], label='diastolic bp')
         plt.errorbar(table.index, table['hr'], yerr = standardDeviation['hr'], label='heart rate')
-        plt.legend(loc='lower right')
+        plt.legend(loc='lower right', bbox_to_anchor=(1.4, 0.2))
         plt.xlabel('Time(hour)')
         plt.ylabel('mmHg, heart rate/min')
         plt.xticks(np.linspace(0,23,24))
@@ -108,5 +117,5 @@ class Blood_pressure():
             plt.savefig('bp_hr/fig/%s_bp_hr_%s'%(self.name, fileName))
         if showImage:
             plt.show()
-        plt.clf()
+        plt.clf() # clean the figure
         return
--- asus_vivo_watch_bp/test.py
+++ asus_vivo_watch_bp/test.py
@@ -1,6 +1,8 @@
 import omnicare_tools.bloodpressure as ob
 df = ob.Blood_pressure('03_bp_df.csv')
 df.showhead(15)
+
+# Test with arbitary file name
 df.preprocess('test22')
 df.lineplot(saveFile=True)
 df.errorbar(saveFile=True)
